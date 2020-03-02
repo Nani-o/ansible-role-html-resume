144193231c298b0929463d8f80dd092538412d12
Sofiane Medjkoune <sofiane@medjkoune.fr>
2020-03-02 14:49:15 +0100

    Generating a PDF with A4 pages

--- files/htmltopdf.js
+++ files/htmltopdf.js
@@ -10,6 +10,24 @@ function getFileUrl(str) {
   return encodeURI("file://" + pathName);
 };
 
+page.paperSize = {
+    format        : "A4",
+    orientation    : "portrait",
+    margin        : { left:"1cm", right:"1cm", top:"1cm", bottom:"1cm" },
+    header        : {
+        height        : "3cm",
+        contents        : phantom.callback(function(pageNum, numPages){
+            return("Mon header : " + pageNum + " / " + numPages);
+        })
+    },
+    footer        : {
+        height        : "1cm",
+        contents        : phantom.callback(function(pageNum, numPages){
+            return("Mon footer : " + pageNum + " / " + numPages);
+        })
+    }
+};
+
 page.viewportSize = { width: 1000, height: 50 };
 page.open(getFileUrl(args[1]), function(){
     page.content = page.content.replace(new RegExp('.*CV.*PDF.*', "gm"), "");
